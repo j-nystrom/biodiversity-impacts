ab96ab2879165a8950d97ad11151cafdbd3eceaa
Jakob Nyström <jakob.ma.nystrom@gmail.com>
2024-03-07 15:22:53 +0100

    predicts task final version

--- core/data/predicts_task.py
+++ core/data/predicts_task.py
@@ -27,16 +27,21 @@ class PredictsProcessingTask:
     """
 
     def __init__(self) -> None:
-        pass
+        """
+        Attributes:
+            predicts_2016 (str):
+            predicts_2016 (str):
+            target_col_order (list[str]):
+            all_predicts_data (str):
+            all_site_coords (str):
+        """
+        self.predicts_2016: str = configs.predicts.predicts_2016
+        self.predicts_2022: str = configs.predicts.predicts_2022
+        self.target_col_order: list[str] = configs.predicts.target_col_order
+        self.all_predicts_data: str = configs.predicts.all_predicts_data
+        self.all_site_coords: str = configs.predicts.all_site_coords
 
-    def run_task(
-        self,
-        source_file_2016: str = configs.predicts.source_file_2016,
-        source_file_2022: str = configs.predicts.source_file_2022,
-        col_order: list[str] = configs.predicts.col_order,
-        concat_data_output_path: str = configs.predicts.concat_data_output_path,
-        site_coord_output_path: str = configs.predicts.site_coord_output_path,
-    ) -> None:
+    def run_task(self) -> None:
         """
         Orchestrate the processing of PREDICTS data, incl. loading,
         concatenating, generating site coordinates and saving dataframes.
@@ -47,33 +52,28 @@ class PredictsProcessingTask:
         3. Generates a geodataframe with coordinates for each sampling site.
         4. Saves both dataframes to disk.
 
-        Args:
-            source_file_2016 (str):
-            source_file_2022 (str):
-            col_order (List[str]):
-            concat_data_output_path (str):
-            site_coord_output_path (str):
-
-        Returns:
-            None
+        TODO: Add asserts that check that the length of the generated frames
+        are correct.
         """
         logger.info("Starting processing of PREDICTS data.")
         start = time.time()
 
         # Load the two PREDICTS datasets (2016 and 2022)
         read_kwargs = {"infer_schema_length": 100000, "null_values": ["NA"]}
-        df_2016 = pl.read_csv(source_file_2016, **read_kwargs)
-        df_2022 = pl.read_csv(source_file_2022, **read_kwargs)
+        df_2016 = pl.read_csv(self.predicts_2016, **read_kwargs)
+        df_2022 = pl.read_csv(self.predicts_2022, **read_kwargs)
 
         # Concatenate them together
-        df_concat = concatenate_predicts_datasets(df_2016, df_2022, col_order=col_order)
+        df_concat = concatenate_predicts_datasets(
+            df_2016, df_2022, col_order=self.target_col_order
+        )
 
         # Generate a geodataframe with coordinates for every sampling site
         gdf_site_coords = create_site_coord_geometries(df_concat)
 
         # Save both dataframes to disk
-        df_concat.write_parquet(concat_data_output_path)
-        gdf_site_coords.to_file(site_coord_output_path)
+        df_concat.write_parquet(self.all_predicts_data)
+        gdf_site_coords.to_file(self.all_site_coords)
 
         runtime = str(timedelta(seconds=int(time.time() - start)))
         logger.info(f"PREDICTS data processing finished in {runtime}.")
