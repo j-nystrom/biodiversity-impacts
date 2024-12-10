12444fee8d25aab98d2d39ece40e2c8864b20d07
Jakob Nyström <jakob.ma.nystrom@gmail.com>
2024-12-10 22:02:15 +0100

    refactored predicts task

--- core/data/predicts_task.py
+++ core/data/predicts_task.py
@@ -5,10 +5,6 @@ from datetime import timedelta
 import polars as pl
 from box import Box
 
-from core.data.data_processing import (
-    concatenate_predicts_datasets,
-    create_site_coord_geometries,
-)
 from core.utils.general_utils import create_logger
 
 # Load config file
@@ -20,58 +16,115 @@ logger = create_logger(__name__)
 
 class PredictsProcessingTask:
     """
-    Task for loading and concatenating PREDICTS datasets, and extracting
-    sampling site coordinates as Point geometries.
+    Task for loading and concatenating the two PREDICTS datasets from 2016 and
+    2022 and ensuring consistency between them.
+
+    Potential (unit) tests (to be implemented):
+    - Check that the concatenated dataframe has the same number of rows as
+        the sum of the two original dataframes.
+    - Test the consistent column logic
+    - Check that rows in the geodataframe match the number of unique sites
     """
 
-    def __init__(self) -> None:
+    def __init__(self, run_folder_path: str) -> None:
         """
         Attributes:
-            predicts_2016: Path to the PREDICTS 2016 data.
-            predicts_2016: Path to the PREDICTS 2016 data.
-            target_col_order: Target column order in concatenated dataframe.
-            all_predicts_data: Output path for the concatenated dataframe.
-            all_site_coords: Output path for geodataframe with site coords.
+            run_folder_path: Folder where logs and certain outputs are stored.
+            predicts_2016_path: Path to the PREDICTS 2016 data.
+            predicts_2022_path: Path to the PREDICTS 2022 data.
+            output_col_order: Desired column order in concatenated dataframe.
+            merged_data_path: Output path for the concatenated dataframe.
         """
-        self.predicts_2016: str = configs.predicts.predicts_2016
-        self.predicts_2022: str = configs.predicts.predicts_2022
-        self.target_col_order: list[str] = configs.predicts.target_col_order
-        self.all_predicts_data: str = configs.predicts.all_predicts_data
-        self.all_site_coords: str = configs.predicts.all_site_coords
+        self.run_folder_path = run_folder_path
+        self.predicts_2016_path: str = configs.predicts.predicts_2016_path
+        self.predicts_2022_path: str = configs.predicts.predicts_2022_path
+        self.output_col_order: list[str] = configs.predicts.output_col_order
+        self.merged_data_path: str = configs.predicts.merged_data_path
 
     def run_task(self) -> None:
         """
-        Orchestrate the processing of PREDICTS data, incl. loading,
-        concatenating, generating site coordinates and saving dataframes.
-
-        This function performs the following steps
-        - Loads the PREDICTS datasets for 2016 and 2022
-        - Concatenates the datasets together
-        - Generates a geodataframe with coordinates for each sampling site
-        - Saves both dataframes to disk.
-
-        TODO: Add asserts that check that the length of the generated frames
-        are correct.
+        The following key processing steps are performed:
+        - Load the PREDICTS datasets for 2016 and 2022
+        - Concatenate the datasets together, ensuring that all columns are
+            consistent and in the correct order
+        - Save dataframe as a parquet file
         """
         logger.info("Starting processing of PREDICTS data.")
         start = time.time()
 
-        # Load the two PREDICTS datasets (2016 and 2022)
+        # Number of rows to read to infer schema, to speed up reading
         read_kwargs = {"infer_schema_length": 100000, "null_values": ["NA"]}
-        df_2016 = pl.read_csv(self.predicts_2016, **read_kwargs)
-        df_2022 = pl.read_csv(self.predicts_2022, **read_kwargs)
 
-        # Concatenate them together
-        df_concat = concatenate_predicts_datasets(
-            df_2016, df_2022, col_order=self.target_col_order
+        # Read and concatenate the two PREDICTS datasets
+        df_2016 = pl.read_csv(self.predicts_2016_path, **read_kwargs)
+        df_2022 = pl.read_csv(self.predicts_2022_path, **read_kwargs)
+        df_concat = self.concatenate_predicts_dataframes(
+            df_2016, df_2022, col_order=self.output_col_order
         )
 
-        # Generate a geodataframe with coordinates for every sampling site
-        gdf_site_coords = create_site_coord_geometries(df_concat)
-
-        # Save both dataframes to disk
-        df_concat.write_parquet(self.all_predicts_data)
-        gdf_site_coords.to_file(self.all_site_coords)
+        # Save dataframe to disk
+        df_concat.write_parquet(self.merged_data_path)
 
         runtime = str(timedelta(seconds=int(time.time() - start)))
         logger.info(f"PREDICTS data processing finished in {runtime}.")
+
+    def concatenate_predicts_dataframes(
+        self, df_2016: pl.DataFrame, df_2022: pl.DataFrame, col_order: list[str]
+    ) -> pl.DataFrame:
+        """
+        Concatenate the two PREDICTS datasets, ensuring identical column
+        structures and specified order.
+
+        Args:
+            df_2016: Dataframe with the PREDICTS dataset from 2016.
+            df_2022: Dataframe with the PREDICTS dataset from 2022.
+            col_order: Column order in the final concatenated dataframe.
+
+        Returns:
+            df_concat: Concatenated dataframe with ordered columns.
+
+        Raises:
+            ValueError: If 'col_order' has extra or missing columns compated to
+                the concatenated dataframe.
+        """
+        logger.info("Concatenating the PREDICTS datasets.")
+
+        # Find out if there are any columns that are not overlapping
+        # Note: These are not important for modeling or analysis
+        unique_cols_2016 = list(set(df_2016.columns) - set(df_2022.columns))
+        unique_cols_2022 = list(set(df_2022.columns) - set(df_2016.columns))
+        all_unique_cols = unique_cols_2016 + unique_cols_2022
+
+        # Drop non-overlapping columns from both dataframes, and make sure we
+        # have the same column order in both dataframes
+        if all_unique_cols:
+            logger.warning(f"Dropping non-overlapping columns: {all_unique_cols}")
+            df_2016 = df_2016.drop(all_unique_cols, strict=False)  # Ignore missing cols
+            df_2022 = df_2022.drop(all_unique_cols, strict=False)
+
+        df_2022 = df_2022.select(df_2016.columns)
+
+        # Append new data to the old df, then sort columns in the right order
+        # Ensure 'col_order' contains all columns present in the dataframe
+        # and that 'col_order' doesn't contain any extra columns not present
+        df_concat = pl.concat([df_2016, df_2022], how="vertical")
+        missing_cols = set(df_concat.columns) - set(col_order)
+        if missing_cols:
+            raise ValueError(
+                f"Missing columns in 'col_order': {missing_cols}, specify all columns."
+            )
+
+        extra_cols = set(col_order) - set(df_concat.columns)
+        if extra_cols:
+            raise ValueError(
+                f"Extra columns in 'col_order': {extra_cols}, remove from list."
+            )
+
+        # Reorder the columns according to 'col_order', and sort data by 'SSBS'
+        df_concat = df_concat.select(col_order)
+        df_concat = df_concat.sort("SSBS", descending=False)
+
+        logger.info(f"Shape of concatenated DataFrame: {df_concat.shape}")
+        logger.info("Finished concatenating PREDICTS datasets.")
+
+        return df_concat
