8c1614d1d79fa02bf1b53ea57f787fa4c46597e3
Jakob Nyström <jakob.ma.nystrom@gmail.com>
2025-11-20 14:13:55 +0100

    small improvements and robustness

--- core/data/predicts_task.py
+++ core/data/predicts_task.py
@@ -5,8 +5,8 @@ from datetime import timedelta
 import polars as pl
 from box import Box
 
-from core.tests.data.validate_data import validate_concatenate_predicts_dataframes
 from core.tests.shared.validate_shared import (
+    check_duplicates,
     get_unique_value_count,
     validate_input_files,
     validate_output_files,
@@ -22,24 +22,24 @@ logger = create_logger(__name__)
 class PredictsConcatenationTask:
     """
     Task for loading and concatenating the two PREDICTS datasets from 2016 and
-    2022 and ensuring consistency between them.
-
-    NOTE: To streamline model analysis, it would make sense to save key
-    information about each site in a separate dataframe.
+    2022 and ensuring consistency between them. Each dataset contains a unique
+    set of studies and sites.
     """
 
     def __init__(self, run_folder_path: str) -> None:
         """
         Attributes:
-            - run_folder_path: Folder for storing logs and certain outputs.
+            - run_folder_path: Folder for storing logs and key outputs.
             - predicts_2016_path: Path to the PREDICTS 2016 data.
             - predicts_2022_path: Path to the PREDICTS 2022 data.
+            - infer_schema_length: Nb of rows for schema inference when loading CSVs.
             - output_col_order: Desired column order in concatenated dataframe.
             - merged_data_path: Output path for the concatenated dataframe.
         """
         self.run_folder_path = run_folder_path
         self.predicts_2016_path: str = configs.predicts.predicts_2016_path
         self.predicts_2022_path: str = configs.predicts.predicts_2022_path
+        self.infer_schema_length: int = configs.predicts.infer_schema_length
         self.output_col_order: list[str] = configs.predicts.output_col_order
         self.merged_data_path: str = configs.predicts.merged_data_path
 
@@ -49,6 +49,7 @@ class PredictsConcatenationTask:
             - Load the PREDICTS datasets for 2016 and 2022.
             - Concatenate the datasets together, ensuring that all columns are
                 consistent and in the correct order.
+            - Validate the concatenated dataframe.
             - Save dataframe as a parquet file.
         """
         logger.info("Starting processing of PREDICTS data.")
@@ -56,7 +57,10 @@ class PredictsConcatenationTask:
 
         # Number of rows used to infer the schema, to speed up reading
         # This was implmented as there were some issues reading the files
-        read_kwargs = {"infer_schema_length": 100000, "null_values": ["NA"]}
+        read_kwargs = {
+            "infer_schema_length": self.infer_schema_length,
+            "null_values": ["NA"],
+        }
 
         # Read and concatenate the two PREDICTS datasets
         validate_input_files(
@@ -72,7 +76,7 @@ class PredictsConcatenationTask:
             f"Number of unique sites: {get_unique_value_count(df_concat, 'SSBS'):,}"
         )
 
-        # Save dataframe to disk
+        # Save dataframe to disk (overwriting the previous file if it exists)
         validate_output_files(
             file_paths=[self.merged_data_path], files=[df_concat], allow_overwrite=True
         )
@@ -100,15 +104,19 @@ class PredictsConcatenationTask:
         logger.info("Concatenating the PREDICTS datasets.")
 
         # Find out if there are any columns that are not overlapping
-        # Note: Not important for modeling or analysis, so can be dropped
         unique_cols_2016 = list(set(df_2016.columns) - set(df_2022.columns))
         unique_cols_2022 = list(set(df_2022.columns) - set(df_2016.columns))
         all_unique_cols = unique_cols_2016 + unique_cols_2022
 
         # Drop non-overlapping columns from both dataframes, and make sure we
-        # have the same column order in both dataframes
+        # have the same column order in both dataframes. None of the identified
+        # columns are important for modeling or analysis, so can be dropped
         if all_unique_cols:
-            logger.warning(f"Dropping non-overlapping columns: {all_unique_cols}")
+            logger.warning(
+                f"Dropping non-overlapping columns: {all_unique_cols}\n"
+                f"Columns only in 2016: {unique_cols_2016}\n"
+                f"Columns only in 2022: {unique_cols_2022}"
+            )
             df_2016 = df_2016.drop(all_unique_cols, strict=False)  # Ignore missing cols
             df_2022 = df_2022.drop(all_unique_cols, strict=False)
 
@@ -122,7 +130,7 @@ class PredictsConcatenationTask:
         )
 
         # Check the results before returning
-        validate_concatenate_predicts_dataframes(
+        self.validate_concatenate_predicts_dataframes(
             df_2016,
             df_2022,
             df_concat,
@@ -132,3 +140,46 @@ class PredictsConcatenationTask:
         logger.info("Finished concatenating PREDICTS datasets.")
 
         return df_concat
+
+    @staticmethod
+    def validate_concatenate_predicts_dataframes(
+        df_2016: pl.DataFrame,
+        df_2022: pl.DataFrame,
+        df_concat: pl.DataFrame,
+        col_order: list[str],
+    ) -> None:
+        """Validate the outputs of the concatenation operations."""
+        # Check for missing or extra columns in the inputted col_order list
+        missing_cols = set(df_concat.columns) - set(col_order)
+        if missing_cols:
+            raise ValueError(
+                f"Missing columns in 'col_order': {missing_cols}, specify all columns."
+            )
+        extra_cols = set(col_order) - set(df_concat.columns)
+        if extra_cols:
+            raise ValueError(
+                f"Extra columns in 'col_order': {extra_cols}, remove from input list."
+            )
+
+        # Check for duplicate rows
+        duplicate_count = check_duplicates(df_concat)
+        if duplicate_count > 0:
+            raise ValueError(f"Duplicate rows detected: {duplicate_count} rows.")
+
+        # Check that the total number of rows adds up
+        expected_rows = df_2016.shape[0] + df_2022.shape[0]
+        if df_concat.shape[0] != expected_rows:
+            raise ValueError(
+                f"Expected {expected_rows} rows, but got {df_concat.shape[0]} rows."
+            )
+
+        # Check that the total number of unique sites add up
+        unique_sites_2016 = get_unique_value_count(df_2016, "SSBS")
+        unique_sites_2022 = get_unique_value_count(df_2022, "SSBS")
+        expected_unique_sites = unique_sites_2016 + unique_sites_2022
+        actual_unique_sites = get_unique_value_count(df_concat, "SSBS")
+        if actual_unique_sites != expected_unique_sites:
+            raise ValueError(
+                f"Unique site count mismatch: Expected {expected_unique_sites} sites, "
+                f"but got {actual_unique_sites} sites."
+            )
